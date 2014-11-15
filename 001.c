bb1d47ef9bd0f1f0884dfba867f9e0082c79a88b
techird <techird@qq.com>
2014-11-15 11:31:22 +0800

    function

--- 001.c
+++ 001.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
+int is_water_flower_number(number)
+{
+    int sum = 0;
+    int x = number;
+    int d;
+    while (x) {
+        d = x % 10;
+        sum += d * d * d;
+        x /= 10;
+    }
+    return sum == number;
+}
+
 int main(void)
 {
-    int i, sum, index, x;
+    int i, sum;
     
-    for(i = 1; i <= 1000; i++)
+    for (i = 1; i <= 1000; i++)
     {
-        index = i;
-        sum = 0;
-        while(1)
+        if (is_water_flower_number(i))
         {
-            x = index % 10;
-            sum = sum + (x * x * x);
-            index = index / 10;
-            if(index == 0) break;
+            printf("Water flower number found: %d\n", i);
         }
-        if(i == sum)
-        {
-            printf("i = %d \nsum = %d\n", i, sum);
-        }
-    
     }
 
 }
