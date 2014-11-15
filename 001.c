ed5478792d3e7eca06d8234240ef856f2fbd99b8
techird <liujiaming@baidu.com>
2014-11-15 10:55:42 +0800

    更新代码的排版

--- 001.c
+++ 001.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main(void)
 {
-    int i , sum=0 ,index ,x;
-        for(i=1;i<=1000;i++)
-        {
-            index = i ;
-            while(1)
+    int i, sum = 0, index, x;
+    
+    for(i = 1; i <= 1000; i++)
+    {
+        index = i ;
+        while(1)
         {
             x = index % 10;
             sum = sum + (x * x * x);
             index = index / 10;
-            if(index == 0)
-                break;
+            if(index == 0) break;
         }
-        if(i==sum)
-            {
+        if(i == sum)
+        {
             printf("i=%d\nsum=%d\n",i,sum);
             break ;
-            }
-
         }
-
+    
+    }
 
 }
